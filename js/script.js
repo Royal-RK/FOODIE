deeac267aa74e6c2ecc290a45d7a1a3f96c30ee7
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-08-17 20:59:14 +0530

    update media

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+// Testimonial slider
+let swiper = new Swiper(".testimoniSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
