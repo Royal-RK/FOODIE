aa087f32449774ea1c7064c4e39ac1d2488379b1
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-08-18 22:38:27 +0530

    fixed some issue

--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
-let swiper = new Swiper(".mySwiper", {
+let bnrSwiper = new Swiper(".bnrSwiper", {
   loop: true,
   autoplay: {
     delay: 3000,
     pauseOnMouseEnter: true,
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".bnr-swiper-pagination",
     clickable: true,
   },
 });
 
 // Testimonial slider
-let swiper = new Swiper(".testimoniSwiper", {
+let testimoniSwiper = new Swiper(".testimoniSwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+  },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".testimoni-swiper-pagination",
     clickable: true,
   },
   breakpoints: {
-    576: {
+    320: {
       slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
       spaceBetween: 40,
     },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // 768: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 40,
+    // },
     992: {
       slidesPerView: 3,
       spaceBetween: 50,
     },
   },
+});
+// Feedback section counter
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  const target = Number(counter.dataset.target);
+  let count = 0;
+
+  function updateCounter() {
+    count += target / 100;
+
+    if (count < target) {
+      counter.textContent = Math.floor(count);
+      requestAnimationFrame(updateCounter);
+    } else {
+      counter.textContent = target;
+    }
+  }
+
+  updateCounter();
 });
